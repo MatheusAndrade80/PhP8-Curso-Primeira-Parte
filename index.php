37bd0114e396eec969edaa41a50279fbf110b30b
unknown <matheusextra_bac@hotmail.com>
2025-01-28 14:15:49 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+//cabeçalho metadados
+include_once './layout/head.php';
+
+//cabeçalho com menu
+include_once './layout/header.php';  
+
+//conteudo
+include_once './layout/main.php';
+
+//scripts
+include_once './layout/scripts.php';
+
+//rodape
+include_once './layout/footer.php';
+
+
